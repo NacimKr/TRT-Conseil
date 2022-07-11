f341902f004ec73399f2d466e6a4ece1eac95219
Nass <nassimkarroum0@gmail.com>
2022-07-11 14:52:33 +0200

    Add a create job a recruteur space

--- views/recruteur/nouvelle_offre.view.php
+++ views/recruteur/nouvelle_offre.view.php
@@ -1,3 +1,11 @@
+<div class="p-5" style="background-image:url('./img/administration.jpg'); height:280px">
+    <div class="fs-2 bg-light fw-bold rounded-circle text-center w-25 mx-auto p-4 mt-5">TRT Conseil</div>
+</div>
+
+<div class="card mx-auto" style="width: 650px; margin-top:-50px;">
+    <div class="card-body fw-bold text-center fs-2 text-uppercase shadow">La liste des offres d'emploi</div>
+</div>
+
 <form action="create_emploi">
     <button type="submit" class="btn btn-danger d-block mx-auto m-5">Publier une nouvelle offre</button>
 </form>
