bc577fc83e76037cf2fc63b20779e73c9daa5616
Nass <nassimkarroum0@gmail.com>
2022-07-11 14:35:54 +0200

    Add a create job a recruteur space

--- views/recruteur/nouvelle_offre.view.php
+++ views/recruteur/nouvelle_offre.view.php
@@ -1,3 +1,21 @@
+<?php foreach($emplois as $emploi) : ?>
+        <div class="card text-center d-inline-block mt-5 mx-3 shadow" style="width:350px">
+            <div class="card-header">
+                <p><strong>Intitulé du poste </strong><br/><?= $emploi['POSTE'] ?></p>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">Description : </h5>
+                <p class="card-text"><?= $emploi['Description'] ?></p>
+                <a href="#" class="btn btn-primary">
+                Salaire : <?= $emploi['Salaire'] ?>€</a>
+            </div>
+            <div class="card-footer text-muted">
+                <div>Durée : <?= $emploi['Durée'] ?></div>
+                <div>Horaires : <?= $emploi['heure_debut'] ?> à <?= $emploi['heure_fin'] ?></div>
+            </div>
+        </div>
+<?php endforeach; ?>
+
 <form action="create_emploi">
-<button type="submit" class="btn btn-danger d-block mx-auto m-5">Publier une nouvelle offre</button>
+    <button type="submit" class="btn btn-danger d-block mx-auto m-5">Publier une nouvelle offre</button>
 </form>
