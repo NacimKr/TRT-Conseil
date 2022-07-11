0e13c5ca66401ae033fcfc2aa6b39e2d93e26a8b
Nass <nassimkarroum0@gmail.com>
2022-07-11 15:19:15 +0200

    Some correctioin in recurteur space in create new job form

--- controllers/MainController.php
+++ controllers/MainController.php
@@ -107,11 +107,21 @@ public function nouvelle_offre(){
 }
 
 public function create_emploi_recr(){
-    $emplois_recr = $this->mainModel->getAllDataFromDB();
+    //$emplois_recr = $this->mainModel->getAllDataFromDB();
     ob_start();
     require_once "./views/recruteur/create_emploi_recr.view.php";
     $page_content = ob_get_clean();
     require_once "./views/common/template.php";
+    if(isset($_POST['poste']) && isset($_POST['duration']) && isset($_POST['description'])){
+        $annoncePublier = $this->mainModel->createNewOffresEmploi($_POST['poste'], $_POST['duration'], $_POST['description'], $_POST['salary'], $_POST['debut'], $_POST['fin']);
+        header('Location:'.URL.'/offres_emploi');
+        if($annoncePublier){
+            $_SESSION['alert'] = [
+                "class" => "alert-primary",
+                "message" => "Votre annonce à bien été publié"
+            ];
+        }
+    }
 }
 
 public function candidats_postule(){
