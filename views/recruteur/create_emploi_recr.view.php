9ab2cf93c58f3fbb861fbc9df8180dad66fa4aa4
Nass <nassimkarroum0@gmail.com>
2022-07-11 15:16:29 +0200

    Some correctioin in recurteur space in create new job form

--- views/recruteur/create_emploi_recr.view.php
+++ views/recruteur/create_emploi_recr.view.php
@@ -1,3 +1,11 @@
+<div class="p-5" style="background-image:url('./img/administration.jpg'); height:280px">
+    <div class="fs-2 bg-light fw-bold rounded-circle text-center w-25 mx-auto p-4 mt-5">TRT Conseil</div>
+</div>
+
+<div class="card mx-auto" style="width: 650px; margin-top:-50px;">
+    <div class="card-body fw-bold text-center fs-2 text-uppercase shadow">Publier votre offre</div>
+</div>
+
 <form class="container mt-5" style="width:700px;" method="POST">
     <div class="mb-3">
         <label for="poste" class="form-label">Intitulé du Poste</label>
